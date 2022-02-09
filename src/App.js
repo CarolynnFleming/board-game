4a28c6f0aec2c2cdf063e4b19af64e8ccfb83eb1
CarolynnFleming <cfcatpink@gmail.com>
2022-02-09 18:14:48 -0500

    state

--- src/App.js
+++ src/App.js
@@ -17,9 +17,11 @@ import { logout } from './services/fetch-utils';
 
 export default function App() {
   // You'll need to track the user in state
-
+  const [user, setUser] = useState(null);
   // add a useEffect to get the user and inject the user object into state on load
+  useEffect(() => {
 
+  }, [])
   async function handleLogout() {
     // call the logout function
     // clear the user in state
