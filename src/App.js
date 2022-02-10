14d7646f530eb15b763075fab102eba3c03fc2fc
CarolynnFleming <cfcatpink@gmail.com>
2022-02-09 19:52:00 -0500

    use effect

--- src/App.js
+++ src/App.js
@@ -17,11 +17,15 @@ import { logout } from './services/fetch-utils';
 
 export default function App() {
   // You'll need to track the user in state
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(localStorage.getItem('supabase.auth.token'));
   // add a useEffect to get the user and inject the user object into state on load
   useEffect(() => {
-
-  }, [])
+    async function fetchUser() {
+      const user = await getUser();
+      setUser(user);
+    }
+    fetchUser();
+  }, []);
   async function handleLogout() {
     // call the logout function
     // clear the user in state
