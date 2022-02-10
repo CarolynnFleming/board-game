bbd6d91427d3cbd8c9cfc1533a1ee5f892ba5d43
CarolynnFleming <cfcatpink@gmail.com>
2022-02-10 12:16:05 -0500

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Board Game/i);
+  const linkElement = screen.getByText(/Boardzo/i);
   expect(linkElement).toBeInTheDocument();
 });
