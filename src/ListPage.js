eb5cedb6e9fdd9e29480872e14e34992913c2520
CarolynnFleming <cfcatpink@gmail.com>
2022-02-10 00:30:36 -0500

    use effect

--- src/ListPage.js
+++ src/ListPage.js
@@ -5,7 +5,17 @@ import Game from './Game';
 export default function ListPage() {
   // you'll need some state to hold onto the array of games
   const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      const fetchGames = await getGames();
+
+      setGames(fetchGames);
+    }
+    fetch();
+  }, []);
   // fetch the games on load and inject them into state
+
   return (
     <div className='list games'>
       {/* map through the games in state and render Game components */}
