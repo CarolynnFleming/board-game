4547cffa57c526a2689b8da4d2a2518be2b8c9df
CarolynnFleming <cfcatpink@gmail.com>
2022-02-09 23:16:54 -0500

    detail done

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -6,7 +6,13 @@ export default function DetailPage() {
   const [game, setGame] = useState({});
   const match = useRouteMatch();
   // on mount, fetch and set in state the correct board game for this id (the id can be found in match.params using the correct react-router hook)
-
+  useEffect(() => {
+    async function fetch() {
+      const gameResponse = await getGameById(match.params.id);
+      setGame(gameResponse);
+    }
+    fetch();
+  }, [match]);
   return (
     <div className='detail'>
       <h1>{game.title}</h1>
